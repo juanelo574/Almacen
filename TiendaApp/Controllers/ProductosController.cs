33cde4d1d19ff861dce4847d13cd2c11c98c8498
Juanelo <jdelcidr2@miumg.edu.gt>
2026-05-24 17:39:21 -0600

    se agregaron los metodos index y crear producto

--- TiendaApp/Controllers/ProductosController.cs
+++ TiendaApp/Controllers/ProductosController.cs
@@ -1,16 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TiendaApp.Models;
 
 namespace TiendaApp.Controllers
 {
     public class ProductosController : Controller
     {
-        public ProductosController()
+        private readonly TiendaContext _context;
+        public ProductosController(TiendaContext context)
         {
-            
+            _context = context;
         }
         public IActionResult Index()
         {
+            var productos = _context.Productos.ToList();
             return View();
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Productos producto)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producto);
         }
     }
 }
